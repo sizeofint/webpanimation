b413f9843fe3563d9f42b1cec125e3642d4c73d7
phoebus <george.imerlishvili@gmail.com>
2021-02-25 00:39:43 +0400

    fix comments

--- examples/simple-animation/main.go
+++ examples/simple-animation/main.go
@@ -56,35 +56,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	webpanim := webpanimation.NewWebpAnimation(1062, 938, 0) // Create 500x500 animaton with loop count 2, pass 0 for endless loop
+	webpanim := webpanimation.NewWebpAnimation(1062, 938, 0)
 	webpanim.WebPAnimEncoderOptions.SetKmin(9)
 	webpanim.WebPAnimEncoderOptions.SetKmax(17)
 	defer webpanim.ReleaseMemory() // dont forget call this or you will have memory leaks
 	webpConfig := webpanimation.NewWebpConfig()
 	webpConfig.SetLossless(1)
 
-	err = webpanim.AddFrame(frame1, 0, webpConfig) // first frame
+	err = webpanim.AddFrame(frame1, 0, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = webpanim.AddFrame(frame2, 1000, webpConfig) // first frame
+	err = webpanim.AddFrame(frame2, 1000, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = webpanim.AddFrame(frame3, 2000, webpConfig) // first frame
+	err = webpanim.AddFrame(frame3, 2000, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = webpanim.AddFrame(frame4, 3000, webpConfig) // first frame
+	err = webpanim.AddFrame(frame4, 3000, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = webpanim.AddFrame(frame5, 4000, webpConfig) // first frame
+	err = webpanim.AddFrame(frame5, 4000, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = webpanim.AddFrame(nil, 5000, webpConfig) // end loop on 6 second and start over
+	err = webpanim.AddFrame(nil, 5000, webpConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
